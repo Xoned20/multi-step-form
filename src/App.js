57e057b16670710d0e82ee3875ff1a3a37b825cc
Jephery Cyril <cyriljeph@gmail.com>
2023-01-05 19:05:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+    </Routes>
+  )
+}
+
+export default App

b8954feb6e72ecc159258d827b1ba9adfdb6266d
Jephery Cyril <cyriljeph@gmail.com>
2023-01-07 06:14:55 +0100

    sidebar desktop done

--- src/components/styles/Flex.styled.js
+++ src/components/styles/Flex.styled.js
@@ -5,13 +5,16 @@ export const Flex = styled.div`
     align-items: center;
 
     @media(max-width: ${({theme}) => theme.mobile}){
-        align-items: center;
+    
     }
 `;
 
 export const FlexSidebar = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: flex-start;
+    padding-left: 2em;
+    padding-top: 2em;
     /* align-items: flex-start; */
     /* padding: 40px 80px 246px 25px; */
     /* width: 276px; */

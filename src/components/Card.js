d0aca8fc4f4d5d34eed6f97372b0be90ab25e02f
Jephery Cyril <cyriljeph@gmail.com>
2023-01-09 18:01:14 +0100

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,6 +11,20 @@ const Card = () => {
           <h3>Arcade</h3>
           <span>$9/mo</span>
         </div>
+        <div>
+          <img src="./images/icon-advanced.svg" alt="" />
+        </div>
+        <div>
+          <h3>Advanced</h3>
+          <span>$12/mo</span>
+        </div>
+        <div>
+          <img src="./images/icon-pro.svg" alt="" />
+        </div>
+        <div>
+          <h3>Pro</h3>
+          <span>$15/mo</span>
+        </div>
     </StyledCard>
   )
 }
